053a2293eb948bd1efd3eb709f8d9f8be98e43cc
john20018956 <hyoungjinlee2019@gmail.com>
2023-04-16 15:01:08 -0500

    changes

--- part1/course_part1/src/App.js
+++ part1/course_part1/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
+      {/* <Header course={course}/> */}
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises3+exercises1+exercises2}/>
     </div>
